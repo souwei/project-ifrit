9f28c88bc31c85a251be9d83c8451584ae1a4216
Soul Wong <sou.wei.wong@gmail.com>
2020-05-30 21:25:24 +0800

    Render list of dungeons in DungeonsGuide component

--- src/components/dungeonsGuide/DungeonsGuide.jsx
+++ src/components/dungeonsGuide/DungeonsGuide.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 import styles from './DungeonsGuide.module.scss';
 
-const DungeonsGuide = () => (
-    <div className={styles.container}>
-        Dungeons Guide
-    </div>
-);
+import { DUNGEONS_LISTING, EXPANSION_TITLES } from '../constants';
+class DungeonsGuide extends React.Component {
+    renderList() {
+        return DUNGEONS_LISTING.map(({exp, name}) => {
+            return (
+                <div>
+                    {EXPANSION_TITLES[exp]} - {name}
+                </div>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className={styles.container}>
+                Dungeons Guide
+                {this.renderList()}
+            </div>
+        );
+    }
+}
 
 export default DungeonsGuide;
