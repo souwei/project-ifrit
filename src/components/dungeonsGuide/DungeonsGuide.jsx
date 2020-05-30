2bbcd1a4815f1aca4455f30aa4e5a62146b60043
Soul Wong <sou.wei.wong@gmail.com>
2020-05-30 20:10:49 +0800

    Added sass to project

--- src/components/dungeonsGuide/DungeonsGuide.jsx
+++ src/components/dungeonsGuide/DungeonsGuide.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import styles from './DungeonsGuide.module.scss';
+
 const DungeonsGuide = () => (
-    <div>
+    <div className={styles.container}>
         Dungeons Guide
     </div>
 );
